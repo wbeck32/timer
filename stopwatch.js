9760aabdd75c9d31b9810644a712be71540a38e2
Wendy Beck <webeck@gmail.com>
2015-01-12 09:29:32 -0800

    Work in progress.

--- stopwatch.js
+++ stopwatch.js
@@ -4,3 +4,6 @@ var timeFunctions = require('./timeFunctions');
 //Create a stopwatch that counts to 10 using setTimeout.
 //Create a stopwatch that counts to 10 using setReminder.
 timeFunctions.countUsingTimeout();
+
+
+timeFunctions.countUsingReminder();

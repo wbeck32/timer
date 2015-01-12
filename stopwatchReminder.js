e9be9d45511ea30ea48a52a962aff2b90b00512d
Wendy Beck <webeck@gmail.com>
2015-01-12 09:47:43 -0800

    Making progress on the various timers and stopwatches. Still to come: unit tests, user prompts and documentation.

---
+++ stopwatchReminder.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+var timeFunctions = require('./timeFunctions');
+
+//Create a stopwatch that counts to 10 using setReminder.
+timeFunctions.countUsingReminder();
---
+++ stopwatchTimeOut.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+var timeFunctions = require('./timeFunctions');
+
+
+//Create a stopwatch that counts to 10 using setTimeout.
+timeFunctions.countUsingTimeout();

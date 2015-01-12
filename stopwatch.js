9d2d4e0601e0c3e4f4dc96000225214532bfb68e
Wendy Beck <webeck@gmail.com>
2015-01-12 09:48:08 -0800

    Deleted.

--- stopwatch.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-var timeFunctions = require('./timeFunctions');
-
-//Create a stopwatch that counts to 10 using setTimeout.
-//Create a stopwatch that counts to 10 using setReminder.
-timeFunctions.countUsingTimeout();
-
-
-timeFunctions.countUsingReminder();
